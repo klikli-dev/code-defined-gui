88b50912903a1f583b7b412054ff1e40e2ea4702
Kli Kli <klikli@outlook.at>
2026-04-22 22:19:23 +0200

    chore: reuse

--- src/main/java/com/klikli_dev/codedefinedgui/gui/TestScreen.java
+++ src/main/java/com/klikli_dev/codedefinedgui/gui/TestScreen.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
 package com.klikli_dev.codedefinedgui.gui;
 
 import net.minecraft.client.gui.components.AbstractWidget;
